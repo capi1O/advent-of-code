f295d490b8cc396b643f7320520f7351a0e29c22
didrip <dri@mess.ms>
2020-12-04 20:35:01 +0100

    day1 part#2

--- day1.js
+++ day1.js
@@ -11,23 +11,32 @@ const numbers =  fs.readFileSync(path.resolve(process.cwd(), 'day1.txt'))
 	.filter(string => string !== '') // filter empty lines
 	.map(stringNumber => parseInt(stringNumber));
 
-let number1, number2;
+let number1, number2, number3;
 
 numbers.every((num1, idx1) => {
-	const remainingNumbersToTry = numbers.slice(idx1 + 1);
 
-	remainingNumbersToTry.every(num2 => {
-		if (num1 + num2 == 2020) {
-			number1 = num1;
-			number2 = num2;
-			return false; // break the loop
-		}
+	const remainingNumbersToTry1 = numbers.slice(idx1 + 1);
+	remainingNumbersToTry1.every((num2, idx2) => {
+
+		const remainingNumbersToTry2 = numbers.slice(idx2 + 1);
+		remainingNumbersToTry2.every(num3 => {
+
+			if (num1 + num2 + num3 == 2020) {
+				number1 = num1;
+				number2 = num2;
+				number3 = num3;
+				return false; // break the loop
+			}
+			else return true; // continue
+		});
+
+		if (number1 && number2 && number3) return false; // break the loop
 		else return true; // continue
 	});
 
-	if (number1 && number2) return false; // break the loop
+	if (number1 && number2 && number3) return false; // break the loop
 	else return true; // continue
 });
 
-console.log(`${number1} + ${number2} = 2020`);
-console.log(`${number1} * ${number2} = ${number1 * number2}`);
+console.log(`${number1} + ${number2} + ${number3} = 2020`);
+console.log(`${number1} x ${number2} x ${number3}  = ${number1 * number2 * number3}`);
